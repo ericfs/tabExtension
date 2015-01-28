6e9e995856df01044aeb4d4f9891be953680a20d
efs26 <eric.steinlauf@gmail.com>
2015-01-27 22:37:04 -0500

    Add alternative binding.

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,11 +1,12 @@
 // Listen for keyboard commands.
 chrome.commands.onCommand.addListener(function(command){
+  console.log(command);
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
     var tab = tabs[0];
     var newIndex = tab.index;
-    if (command == 'move-tab-right') {
+    if (command.indexOf('right') >= 0) {
       newIndex += 1;
-    } else if (command == 'move-tab-left') {
+    } else if (command.indexOf('left')) {
       newIndex = Math.max(newIndex - 1, 0);
     }
     chrome.tabs.move(tab.id, {index: newIndex});
