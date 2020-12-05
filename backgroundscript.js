69781f2678079632fdbb0d9da893f3274356639c
ericfs <851850+ericfs@users.noreply.github.com>
2020-12-05 08:52:34 -0500

    Add command to send the active tab to a new window with the same size and position.

--- backgroundscript.js
+++ backgroundscript.js
@@ -6,9 +6,19 @@ chrome.commands.onCommand.addListener(function(command){
     var newIndex = tab.index;
     if (command.indexOf('right') >= 0) {
       newIndex += 1;
-    } else if (command.indexOf('left')) {
+    } else if (command.indexOf('left') >= 0) {
       newIndex = Math.max(newIndex - 1, 0);
+    } else if (command == 'new-window') {
+      sendToNewWindow(tab);
+      return;
     }
     chrome.tabs.move(tab.id, {index: newIndex});
   });
 });
+
+function sendToNewWindow(tab) {
+  chrome.windows.get(tab.windowId, (w) => {
+    const {top, left, width, height} = w;
+    chrome.windows.create({tabId: tab.id, top, left, width, height});
+  });
+}
